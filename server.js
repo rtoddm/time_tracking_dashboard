dbf69719d2be03ed5dffbff7e17c4d6054b8c88d
Todd Moussallem <rtoddm@gmail.com>
2026-09-02 09:00:32 -0500

    Configure port for deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
